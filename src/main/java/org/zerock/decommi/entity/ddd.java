8aa7405e94e7ac91676615e9dfb01ca986177615
Taeil <102219209+k1k2brz@users.noreply.github.com>
2022-08-09 22:59:11 +0900

    taeil

---
+++ src/main/java/org/zerock/decommi/entity/ddd.java
@@ -0,0 +1,26 @@
+package org.zerock.decommi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "d_member")
+public class ddd {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long dno;
+}
