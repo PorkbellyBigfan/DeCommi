b918c1d7377a82f91e0b7fcfbf99dafd495e9498
김형준 <porkbellyweb@gmail.com>
2022-08-09 22:15:10 +0900

    'memberentity'

---
+++ src/main/java/org/zerock/decommi/entity/Member.java
@@ -0,0 +1,25 @@
+package org.zerock.decommi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "d_member")
+public class Member {
+    @Id
+    private String email;
+    private String pw;
+    private String name;
+}
