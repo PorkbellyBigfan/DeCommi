94e1f96cfba15171c0bd6edbae93e5a0e6e8af86
psm418 <106363902+psm418@users.noreply.github.com>
2022-08-24 03:40:17 +0900

    Conflict bug fix

--- src/test/java/org/zerock/decommi/repository/BookmarkRepositoryTests.java
+++ src/test/java/org/zerock/decommi/repository/BookmarkRepositoryTests.java
@@ -1,26 +1,25 @@
-// package org.zerock.decommi.repository;
+package org.zerock.decommi.repository;
 
-// import java.util.stream.IntStream;
+import java.util.stream.IntStream;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.zerock.decommi.entity.Bookmark;
-// import org.zerock.decommi.entity.Diary;
-// import org.zerock.decommi.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.decommi.entity.Bookmark;
+import org.zerock.decommi.entity.Diary;
+import org.zerock.decommi.entity.Member;
 
-// @SpringBootTest
-// public class BookmarkRepositoryTests {
-//     @Autowired
-//     BookmarkRepository bookmarkRepository;
+@SpringBootTest
+public class BookmarkRepositoryTests {
+    @Autowired
+    BookmarkRepository bookmarkRepository;
 
-//     @Autowired
-//     MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
-//     @Autowired
-//     DiaryRepository diaryRepository;
+    @Autowired
+    DiaryRepository diaryRepository;
 
-<<<<<<< HEAD
     @Test
     public void insertBookmarkDummies() {
         IntStream.rangeClosed(1, 100).forEach(i -> {
@@ -29,26 +28,14 @@
             Member writer = Member.builder().email("user" + mno +
                     "@decommi.com").build();
             Diary diary = Diary.builder().dino(dino).build();
-=======
-//     @Test
-//     public void insertBookmarkDummies() {
-//         IntStream.rangeClosed(1, 100).forEach(i -> {
-//             Long mno = (long) (Math.random() * 100) + 1;
-//             Long dino = (long) (Math.random() * 50) + 1;
-//             Member writer = Member.builder().email("user" + mno +
-//                     "@decommi.com").build();
-//             memberRepository.save(writer);
-//             Diary diary = Diary.builder().dino(dino).build();
-//             diaryRepository.save(diary);
->>>>>>> 86f259577e2bd2105f97c1bc3bc0df6ff2de4bb2
 
-//             Bookmark bookmark = Bookmark.builder()
-//                     .bfolderName("folderName " + i)
-//                     .isBookmark(false)
-//                     .user(writer)
-//                     .diary(diary)
-//                     .build();
-//             bookmarkRepository.save(bookmark);
-//         });
-//     }
-// }
+            Bookmark bookmark = Bookmark.builder()
+                    .bfolderName("folderName " + i)
+                    .isBookmark(false)
+                    .user(writer)
+                    .diary(diary)
+                    .build();
+            bookmarkRepository.save(bookmark);
+        });
+    }
+}
--- src/test/java/org/zerock/decommi/repository/HelpRepositoryTests.java
+++ src/test/java/org/zerock/decommi/repository/HelpRepositoryTests.java
@@ -1,8 +1,7 @@
-// package org.zerock.decommi.repository;
+package org.zerock.decommi.repository;
 
-// import java.util.stream.IntStream;
+import java.util.stream.IntStream;
 
-<<<<<<< HEAD:src/test/java/org/zerock/decommi/repository/HelpRepositoryTests.java
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,35 +33,3 @@ public class HelpRepositoryTests {
         });
     }
 }
-=======
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.zerock.decommi.entity.Member;
-// import org.zerock.decommi.entity.ServiceCenter;
-
-// @SpringBootTest
-// public class ServiceCenterRepositoryTests {
-//     @Autowired
-//     ServiceCenterRepository scRepository;
-//     @Autowired
-//     MemberRepository memberRepository;
-
-//     @Test
-//     public void insertSCDummies() {
-//         IntStream.rangeClosed(1, 100).forEach(i -> {
-//             Member writer = Member.builder().email("user" + i + "@decommi.com").build();
-
-//             int count = (int) (Math.random() * 5) + 1;
-//             for (int j = 0; j < count; j++) {
-//                 ServiceCenter serviceCenter = ServiceCenter.builder()
-//                         .writer(writer)
-//                         .title("title" + j)
-//                         .content("content" + j)
-//                         .build();
-//                 scRepository.save(serviceCenter);
-//             }
-//         });
-//     }
-// }
->>>>>>> 86f259577e2bd2105f97c1bc3bc0df6ff2de4bb2:src/test/java/org/zerock/decommi/repository/ServiceCenterRepositoryTests.java
