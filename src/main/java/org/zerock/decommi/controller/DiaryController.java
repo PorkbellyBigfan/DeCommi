c1ee8e9a2adff23bde5b08a3c0f91d639f8aa905
PorkbellyBigfan <porkbellyweb@gmail.com>
2022-09-22 15:44:04 +0900

    test

--- src/main/java/org/zerock/decommi/controller/DiaryController.java
+++ src/main/java/org/zerock/decommi/controller/DiaryController.java
@@ -15,7 +15,7 @@
 // import lombok.extern.log4j.Log4j2;
 
 // @RestController
-// @RequestMapping("/api/")
+// @RequestMapping("/api/diary")
 // @Log4j2
 // @RequiredArgsConstructor
 // public class DiaryController {
