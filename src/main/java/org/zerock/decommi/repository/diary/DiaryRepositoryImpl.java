87335dd7f16dd6e6b82daeb17a4665dbec88e222
김형준 <porkbellyweb@gmail.com>
2022-09-26 20:42:24 +0900

    solved login info problem

--- src/main/java/org/zerock/decommi/repository/diary/DiaryRepositoryImpl.java
+++ src/main/java/org/zerock/decommi/repository/diary/DiaryRepositoryImpl.java
@@ -1,28 +1,28 @@
-package org.zerock.decommi.repository.diary;
+// package org.zerock.decommi.repository.diary;
 
-import java.util.List;
+// import java.util.List;
 
-import javax.persistence.EntityManager;
+// import javax.persistence.EntityManager;
 
-import org.zerock.decommi.entity.diary.Diary;
-import org.zerock.decommi.entity.diary.QDiary;
-import org.zerock.decommi.vo.SearchCondition;
+// import org.zerock.decommi.entity.diary.Diary;
+// import org.zerock.decommi.entity.diary.QDiary;
+// import org.zerock.decommi.vo.SearchCondition;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+// import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-public class DiaryRepositoryImpl implements DiaryCustomRepository {
-    private final JPAQueryFactory jpaQueryFactory;
+// @RequiredArgsConstructor
+// public class DiaryRepositoryImpl implements DiaryCustomRepository {
+// private final JPAQueryFactory jpaQueryFactory;
 
-    @Override
-    public List<Diary> search(SearchCondition searchCondition) {
-        Diary diary = jpaQueryFactory
-                .selectFrom(QDiary.diary)
-                .where(QDiary.diary.dino.eq(dino))
-                .fetch();
-        return;
-    }
+// // @Override
+// // public List<Diary> search(SearchCondition searchCondition) {
+// // Diary diary = jpaQueryFactory
+// // .selectFrom(QDiary.diary)
+// // .where(QDiary.diary.dino.eq(dino))
+// // .fetch();
+// // return;
+// // }
 
-}
+// }
