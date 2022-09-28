569139f01efdbf7300bb2c0dc007a2909202b7c0
psm418 <106363902+psm418@users.noreply.github.com>
2022-09-28 16:45:32 +0900

    ggg

--- src/main/java/org/zerock/decommi/vo/diarypost.java
+++ src/main/java/org/zerock/decommi/vo/diarypost.java
@@ -8,27 +8,27 @@ import lombok.Data;
 
 @Data
 public class DiaryPost {
-  private Long dino;
-  private String writer;
-  private String title;
-  private String content;
-  private Long replyCnt;
-  private Long heartCnt;
-  private Long bookmarkCnt;
-  private Long reportCnt;
-  private LocalDateTime regDate, modDate;
+    private Long dino;
+    private String writer;
+    private String title;
+    private String content;
+    private Long replyCnt;
+    private Long heartCnt;
+    private Long bookmarkCnt;
+    private Long reportCnt;
+    private LocalDateTime regDate, modDate;
 
-  public DiaryPost(DiaryRepository.getDiaryPost dp) {
-    this.dino = dp.getDino();
-    this.writer = dp.getWriter();
-    this.title = dp.getTitle();
-    this.content = dp.getContent();
-    this.replyCnt = dp.getReplyCnt();
-    this.heartCnt = dp.getHeartCnt();
-    this.bookmarkCnt = dp.getBookmarkCnt();
-    this.reportCnt = dp.getReportCnt();
-    this.regDate = dp.getRegDate();
-    this.modDate = dp.getModDate();
-  }
+    public DiaryPost(DiaryRepository.getDiaryPost dp) {
+        this.dino = dp.getDino();
+        this.writer = dp.getWriter();
+        this.title = dp.getTitle();
+        this.content = dp.getContent();
+        this.replyCnt = dp.getReplyCnt();
+        this.heartCnt = dp.getHeartCnt();
+        this.bookmarkCnt = dp.getBookmarkCnt();
+        this.reportCnt = dp.getReportCnt();
+        this.regDate = dp.getRegDate();
+        this.modDate = dp.getModDate();
+    }
 
 }
