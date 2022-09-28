7317d6f73c761a6bf9c8e1c28154a2f64c39baa7
PorkbellyBigfan <porkbellyweb@gmail.com>
2022-09-28 10:11:05 +0900

    save for search implementation

--- src/main/java/org/zerock/decommi/repository/diary/DiaryRepositoryImpl.java
+++ src/main/java/org/zerock/decommi/repository/diary/DiaryRepositoryImpl.java
@@ -1,40 +1,48 @@
-// package org.zerock.decommi.repository.diary;
+package org.zerock.decommi.repository.diary;
 
-// import java.util.List;
+import java.util.List;
 
-// import javax.persistence.EntityManager;
+import javax.persistence.EntityManager;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.data.domain.Page;
-// import
-// org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-// import org.springframework.stereotype.Repository;
-// import org.springframework.transaction.annotation.Transactional;
-// import org.zerock.decommi.entity.diary.Diary;
-// import org.zerock.decommi.entity.diary.QDiary;
-// import org.zerock.decommi.entity.diary.QTag;
-// import org.zerock.decommi.entity.member.QMember;
-// import org.zerock.decommi.vo.SearchCondition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.decommi.dto.DiaryDTO;
+import org.zerock.decommi.entity.diary.Diary;
+import org.zerock.decommi.entity.diary.QDiary;
+import static org.zerock.decommi.entity.diary.QDiary.diary;
+import org.zerock.decommi.entity.diary.QTag;
+import org.zerock.decommi.vo.DiaryPostList;
+import org.zerock.decommi.vo.SearchCondition;
 
-// import com.querydsl.core.BooleanBuilder;
-// import com.querydsl.core.types.dsl.BooleanExpression;
-// import com.querydsl.jpa.JPQLQuery;
-// import com.querydsl.jpa.impl.JPAQuery;
-// import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.log4j.Log4j2;
+import lombok.RequiredArgsConstructor;
 
-// @Transactional(readOnly = true)
-// @Log4j2
-// public class DiaryRepositoryImpl extends QuerydslRepositorySupport implements
-// DiaryCustomRepository {
+@Repository
+@Transactional(readOnly = true) 
+public class DiaryRepositoryImpl extends QuerydslRepositorySupport implements DiaryCustomRepository {
+  @Autowired
+  JPAQueryFactory queryFactory;
+  
+  public DiaryRepositoryImpl() {
+    super(Diary.class);
+  }
 
-// public DiaryRepositoryImpl() {
-// super(Diary.class);
-// }
 
-// QDiary qDiary = QDiary.diary;
-// QMember qMember = QMember.member;
-// QTag qTag = QTag.tag;
-// }
+  @Override
+  public List<DiaryPostList> getSearch(SearchCondition searchCondition) {
+    JPQLQuery<DiaryPostList> query = queryFactory.selectFrom(diary);
+        
+    
+    return null;
+  }
+
+
+}
--- src/main/java/org/zerock/decommi/vo/SearchCondition.java
+++ src/main/java/org/zerock/decommi/vo/SearchCondition.java
@@ -7,17 +7,10 @@ import lombok.Data;
 @Data
 public class SearchCondition {
   String search;
-  String type;
-  boolean searchType; // 0 일때는 모든 태그 포함된 게시글만 검색, 1일때는 태그가 하나라도 포함된 게시글 전부 검색
   List<String> tagList;
-
-  public SearchCondition(String search, String type) {
+  
+  public SearchCondition(String search, List<String>tagList){
     this.search = search;
-    this.type = type;
-  }
-  public SearchCondition(String search, boolean searchType, List<String>tagList){
-    this.search = search;
-    this.searchType = searchType;
     this.tagList = tagList;
   }
 }
