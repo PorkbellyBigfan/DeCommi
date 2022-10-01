64cda456c9fcf07e848522643e2c690f2c29599a
김형준 <porkbellyweb@gmail.com>
2022-10-01 16:46:42 +0900

    getMyDiaryList

--- src/main/java/org/zerock/decommi/controller/MyDiaryController.java
+++ src/main/java/org/zerock/decommi/controller/MyDiaryController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.decommi.dto.DiaryDTO;
+import org.zerock.decommi.dto.LikeTagListDTO;
 import org.zerock.decommi.dto.PageRequestDTO;
 import org.zerock.decommi.dto.PageResultDTO;
 import org.zerock.decommi.entity.diary.Diary;
+import org.zerock.decommi.entity.member.LikeTagList;
 import org.zerock.decommi.service.diary.MyDiaryService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,11 +29,21 @@ public class MyDiaryController {
   // private final String secretKey = "decommi1q2w3e4r!";
 
   @RequestMapping(value = "/list", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<List<DiaryDTO>> getMyDiaryList(@ModelAttribute PageRequestDTO dto, String id) {
+  public ResponseEntity<List<DiaryDTO>> getMyDiaryList(@ModelAttribute PageRequestDTO dto) {
     PageRequestDTO.builder()
-        .page(10).size(30).type(dto.getType()).keyword(dto.getKeyword())
+        .page(10).size(30).type(dto.getType()).id(dto.getId()).keyword(dto.getKeyword())
         .tagList(dto.getTagList()).build();
     PageResultDTO<DiaryDTO, Diary> result = mdService.getMyDiaryPostList(dto);
     return new ResponseEntity<>(result.getDtoList(), HttpStatus.OK);
   }
+
+  // @RequestMapping(value = "/liketaglist", method = RequestMethod.POST, consumes
+  // = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  // public ResponseEntity<List<DiaryDTO>> getDiaryList(@ModelAttribute
+  // LikeTagListDTO dto) {
+  // PageRequestDTO.builder().page(10).size(30).type(dto.getType()).keyword(dto.getKeyword()).tagList(dto.getTagList()).build();
+  // PageResultDTO<DiaryDTO, Diary> result = mdService.LikeTagList(dto);
+  // return new ResponseEntity<>(result.getDtoList(), HttpStatus.OK);
+  // }
+
 }
