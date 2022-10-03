3b1dedf39f33711f22b4c37c725eee97959801ba
psm418 <106363902+psm418@users.noreply.github.com>
2022-10-03 15:57:36 +0900

    주석

--- src/main/java/org/zerock/decommi/controller/DiaryApiController.java
+++ src/main/java/org/zerock/decommi/controller/DiaryApiController.java
@@ -155,46 +155,53 @@ public class DiaryApiController {
         return new ResponseEntity<>(diaryService.addDiaryReport(dto), HttpStatus.OK);
     }
 
-    // // 댓글페이지
-    // @RequestMapping(value = "/reply/", method = RequestMethod.POST, consumes =
-    // MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<HashMap<String, Object>> replyRead(@RequestBody Reply2
-    // reply2) {
-    // Pageable pageable = PageRequest.of(reply2.getReqPage(), 5);
-    // HashMap<String, Object> result =
-    // diaryService.getReplyListByDino(reply2.getDino(), pageable);
-    // return new ResponseEntity<>(result, HttpStatus.OK);
-    // }
+    // 댓글페이지
+    @RequestMapping(value = "/reply/", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<HashMap<String, Object>> replyRead(@RequestBody Reply2 reply2) {
+        // Reply2
+        // private int reqPage; //페이지
+        // private Long dino; //다이어리 번호
+        // private Long mid; //멤버 번호
+        Pageable pageable = PageRequest.of(reply2.getReqPage(), 5); // 페이징처리
+        HashMap<String, Object> result = diaryService.getReplyListByDino(reply2.getDino(), pageable);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-    // // 댓글
-    // @RequestMapping(value = "/reply/add/", method = RequestMethod.POST, consumes
-    // = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Long> registerReply(@RequestBody ReplyDTO dto) {
-    // Long result = diaryService.registerReply(dto);
-    // return new ResponseEntity<>(result, HttpStatus.OK);
-    // }
+    // ReplyDTO
+    // private Long rno; 댓글번호
+    // private Long Mid; // Member id
+    // private Long dino; // Diary dino
+    // private String replyContent; 댓글내용
+    // private Long replyGroup; 댓글그룹
+    // private Long replyDepth; 대댓글
+    // private Long replyOrder; 댓글위치
+    // private LocalDateTime regDate, modDate;
 
-    // // 대댓글
-    // @RequestMapping(value = "/reply/add/reply", method = RequestMethod.POST,
-    // consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Long> addReply(@RequestBody ReplyDTO dto) {
-    // Long result = diaryService.addNewReply(dto);
-    // return new ResponseEntity<>(result, HttpStatus.OK);
-    // }
+    // 댓글추가
+    @RequestMapping(value = "/reply/add/", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> registerReply(@RequestBody ReplyDTO dto) {
+        Long result = diaryService.registerReply(dto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-    // // 댓글삭제
-    // @RequestMapping(value = "/reply/remove", method = RequestMethod.POST,
-    // consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<String> deleteReply(@RequestBody ReplyDTO dto) {
-    // return new ResponseEntity<>(diaryService.deleteReply(dto), HttpStatus.OK);
-    // }
+    // 대댓글추가
+    @RequestMapping(value = "/reply/add/reply", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> addReply(@RequestBody ReplyDTO dto) {
+        Long result = diaryService.addNewReply(dto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-    // // 댓글수정
-    // @RequestMapping(value = "/reply/modify", method = RequestMethod.POST,
-    // consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<String> modifyReply(@RequestBody ReplyDTO dto) {
-    // String result = diaryService.modifyReply(dto);
-    // return new ResponseEntity<>(result, HttpStatus.OK);
-    // }
+    // 댓글삭제
+    @RequestMapping(value = "/reply/remove", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> deleteReply(@RequestBody ReplyDTO dto) {
+        return new ResponseEntity<>(diaryService.deleteReply(dto), HttpStatus.OK);
+    }
+
+    // 댓글수정
+    @RequestMapping(value = "/reply/modify", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> modifyReply(@RequestBody ReplyDTO dto) {
+        String result = diaryService.modifyReply(dto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
 }
