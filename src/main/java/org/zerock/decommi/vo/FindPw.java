c23902a3a656692a8cfa202dc192981ed0c10646
김형준 <porkbellyweb@gmail.com>
2022-10-16 22:14:08 +0900

    test

--- src/main/java/org/zerock/decommi/vo/FindPw.java
+++ src/main/java/org/zerock/decommi/vo/FindPw.java
@@ -5,9 +5,9 @@ import lombok.Data;
 @Data
 public class FindPw {
     String email;
+    Long mid;
     String q1;
     String q2;
     String q3;
-    String changePw1;
-    String changePw2;
+    String changePw;
 }
